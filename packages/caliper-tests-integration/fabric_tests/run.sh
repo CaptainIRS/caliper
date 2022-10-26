abac822163a69cba6185e3bbfe5d9051bdda7138
CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>
2022-10-26 16:16:06 +0530

    Experimental integration test runner using microfab
    Signed-off-by: CaptainIRS <36656347+CaptainIRS@users.noreply.github.com>

--- packages/caliper-tests-integration/fabric_tests/run.sh
+++ packages/caliper-tests-integration/fabric_tests/run.sh
@@ -20,12 +20,108 @@ set -v
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 
-if [[ ! -d "fabric-samples" ]]; then
-  curl -sSL -k https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/bootstrap.sh | bash -s -- 2.4.3
+if [[ ! -d "bin" || ! -d "config" ]]; then
+  curl -sSL -k https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/bootstrap.sh | bash -s -- 2.4.3 -d -s
 fi
 
-# back to this dir
-cd ${DIR}
+npm install -g @hyperledger-labs/weft
+
+export PATH=${DIR}/bin:$PATH
+
+export MICROFAB_CONFIG='{
+    "ordering_organizations": {
+        "name": "Orderer"
+    },
+    "endorsing_organizations":[
+        {
+            "name": "Org1"
+        },
+        {
+            "name": "Org2"
+        }
+    ],
+    "channels":[
+        {
+            "name": "mychannel",
+            "endorsing_organizations":[
+                "Org1",
+                "Org2"
+            ]
+        },
+        {
+            "name": "yourchannel",
+            "endorsing_organizations":[
+                "Org1",
+                "Org2"
+            ]
+        }
+    ]
+}'
+export CHAINCODE_NAME="marbles"
+
+export CFG=$DIR/_cfg/uf
+rm -rf $CFG
+mkdir -p $CFG
+
+docker run --name microfab -p 8080:8080 --add-host host.docker.internal:host-gateway --rm -d -e MICROFAB_CONFIG="${MICROFAB_CONFIG}"  ibmcom/ibp-microfab:0.0.16
+sleep 5
+
+curl -s http://console.127-0-0-1.nip.io:8080/ak/api/v1/components | weft microfab -w $CFG/_wallets -p $CFG/_gateways -m $CFG/_msp -f
+
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_MSPCONFIGPATH=$CFG/_msp/Org1/org1admin/msp
+export CORE_PEER_ADDRESS=org1peer-api.127-0-0-1.nip.io:8080
+export FABRIC_CFG_PATH=$DIR/config
+export CORE_PEER_CLIENT_CONNTIMEOUT=15s
+export CORE_PEER_DELIVERYCLIENT_CONNTIMEOUT=15s
+
+pushd $DIR/src/${CHAINCODE_NAME}/node
+
+peer lifecycle chaincode package ${CHAINCODE_NAME}.tar.gz --path . --lang node --label ${CHAINCODE_NAME}
+
+export CHAINCODE_ID=$(peer lifecycle chaincode calculatepackageid ${CHAINCODE_NAME}.tar.gz)
+echo "Chaincode ID: ${CHAINCODE_ID}"
+
+peer lifecycle chaincode install ${CHAINCODE_NAME}.tar.gz
+
+peer lifecycle chaincode approveformyorg --channelID mychannel --name mymarbles -v v0 --package-id $CHAINCODE_ID --sequence 1 --connTimeout 15s --signature-policy "OR('Org1MSP.member','Org2MSP.member')"
+peer lifecycle chaincode approveformyorg --channelID yourchannel --name yourmarbles -v v0 --package-id $CHAINCODE_ID --sequence 1 --connTimeout 15s --signature-policy "OR('Org1MSP.member','Org2MSP.member')"
+
+export CORE_PEER_LOCALMSPID=Org2MSP
+export CORE_PEER_MSPCONFIGPATH=$CFG/_msp/Org2/org2admin/msp
+export CORE_PEER_ADDRESS=org2peer-api.127-0-0-1.nip.io:8080
+
+peer lifecycle chaincode install ${CHAINCODE_NAME}.tar.gz
+
+peer lifecycle chaincode approveformyorg --channelID mychannel --name mymarbles -v v0 --package-id $CHAINCODE_ID --sequence 1 --connTimeout 15s --signature-policy "OR('Org1MSP.member','Org2MSP.member')"
+peer lifecycle chaincode approveformyorg --channelID yourchannel --name yourmarbles -v v0 --package-id $CHAINCODE_ID --sequence 1 --connTimeout 15s --signature-policy "OR('Org1MSP.member','Org2MSP.member')"
+
+peer lifecycle chaincode commit --channelID mychannel --name mymarbles -v v0 --sequence 1  --connTimeout 15s --signature-policy "OR('Org1MSP.member','Org2MSP.member')"
+peer lifecycle chaincode querycommitted --channelID=mychannel
+
+peer lifecycle chaincode commit --channelID yourchannel --name yourmarbles -v v0 --sequence 1  --connTimeout 15s --signature-policy "OR('Org1MSP.member','Org2MSP.member')"
+peer lifecycle chaincode querycommitted --channelID=yourchannel
+
+popd
+
+curl -s http://console.127-0-0-1.nip.io:8080/ak/api/v1/components | weft microfab -w $CFG/_wallets -p $CFG/_gateways -m $CFG/_msp -f
+
+dispose () {
+    JOBS=$(jobs -p)
+    if [ -n "$JOBS" ]; then
+        echo "Killing jobs: $JOBS"
+        kill $JOBS
+    fi
+    docker ps -a
+    docker logs microfab > /tmp/microfab.log
+    docker stop microfab
+    ${CALL_METHOD} launch manager --caliper-workspace phase6 --caliper-flow-only-end
+}
+
+trap 'dispose' EXIT
+
+# change default settings (add config paths too)
+export CALIPER_PROJECTCONFIG=../caliper.yaml
 
 # bind during CI tests, using the package dir as CWD
 # Note: do not use env variables for binding settings, as subsequent launch calls will pick them up and bind again
@@ -35,35 +131,17 @@ export NODE_PATH="$SUT_DIR/cached/v$FABRIC_VERSION/node_modules"
 if [[ "${BIND_IN_PACKAGE_DIR}" = "true" ]]; then
     mkdir -p $SUT_DIR/cached/v$FABRIC_VERSION
     pushd $SUT_DIR/cached/v$FABRIC_VERSION
-    ${CALL_METHOD} bind --caliper-bind-sut fabric:$FABRIC_VERSION
+    ${CALL_METHOD} bind \
+        --caliper-bind-sut fabric:$FABRIC_VERSION \
+        --caliper-bind-args="--prefix $SUT_DIR/cached/v$FABRIC_VERSION"
     popd
 fi
-
-# change default settings (add config paths too)
-export CALIPER_PROJECTCONFIG=../caliper.yaml
-
-dispose () {
-    docker ps -a
-    ${CALL_METHOD} launch manager --caliper-workspace phase6 --caliper-flow-only-end
-}
-
-TEST_NETWORK_DIR=${DIR}/fabric-samples/test-network
-
-# Create Fabric network
-pushd ${TEST_NETWORK_DIR}
-./network.sh up -s couchdb
-./network.sh createChannel -c mychannel
-./network.sh createChannel -c yourchannel
-./network.sh deployCC -ccn mymarbles -c mychannel -ccp ${DIR}/src/marbles/node -ccl javascript -ccv v0 -ccep "OR('Org1MSP.member','Org2MSP.member')"
-./network.sh deployCC -ccn yourmarbles -c yourchannel -ccp ${DIR}/src/marbles/node -ccl javascript -ccv v0 -ccep "OR('Org1MSP.member','Org2MSP.member')"
-popd
 
 # PHASE 1: just starting the network
 ${CALL_METHOD} launch manager --caliper-workspace phase1 --caliper-flow-only-start
 rc=$?
 if [[ ${rc} != 0 ]]; then
     echo "Failed CI step 1";
-    dispose;
     exit ${rc};
 fi
 
@@ -72,7 +150,6 @@ ${CALL_METHOD} launch manager --caliper-workspace phase2 --caliper-flow-only-tes
 rc=$?
 if [[ ${rc} != 0 ]]; then
     echo "Failed CI step 2";
-    dispose;
     exit ${rc};
 fi
 
@@ -81,7 +158,6 @@ ${CALL_METHOD} launch manager --caliper-workspace phase3 --caliper-flow-only-tes
 rc=$?
 if [[ ${rc} != 0 ]]; then
     echo "Failed CI step 3";
-    dispose;
     exit ${rc};
 fi
 
@@ -102,7 +178,6 @@ ${CALL_METHOD} launch manager --caliper-workspace phase4 --caliper-flow-only-tes
 rc=$?
 if [[ ${rc} != 0 ]]; then
     echo "Failed CI step 4";
-    dispose;
     exit ${rc};
 fi
 
@@ -123,19 +198,5 @@ ${CALL_METHOD} launch manager --caliper-workspace phase5 --caliper-flow-only-tes
 rc=$?
 if [[ ${rc} != 0 ]]; then
     echo "Failed CI step 5";
-    dispose;
     exit ${rc};
 fi
-
-# PHASE 6: just disposing of the network
-${CALL_METHOD} launch manager --caliper-workspace phase6 --caliper-flow-only-end
-rc=$?
-if [[ ${rc} != 0 ]]; then
-    echo "Failed CI step 6";
-    exit ${rc};
-fi
-
-# Cleanup Fabric network
-pushd ${TEST_NETWORK_DIR}
-./network.sh down
-popd
